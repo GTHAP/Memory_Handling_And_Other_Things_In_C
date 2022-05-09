8f427391743393f6e059f03d82776cb8a58c8802
GTHAP <40315835+GTHAP@users.noreply.github.com>
2022-05-09 18:00:42 +0530

    Create Charachters_in_C_2.c

---
+++ Charachters_in_C_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char name[] = "ICON OF SIN";
+    printf("%p\n", &name); // memory address of charcter array - name
+    printf("%s\n", name); // value stored in name
+    int size = sizeof(name); 
+    printf("%d\n", size); // size of name
+    int len = strlen(name);
+    printf("%d\n", len); // length of string
+    
+    char *ptr = name; // character pointer variable that points to the initial memory address of the array - name
+    printf("%p\n", ptr);
+    printf("%c\n", *ptr);
+    printf("%d\n", *ptr);
+    *ptr = 'U'; // modifying the initial value in the array by changing the value of ptr to U
+    printf("%c\n", *ptr);
+    printf("%s\n", name);
+}
