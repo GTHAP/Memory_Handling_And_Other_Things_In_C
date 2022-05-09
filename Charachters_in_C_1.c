750ed8533a12ece67c5b8178836bcc24a75849ed
GTHAP <40315835+GTHAP@users.noreply.github.com>
2022-05-09 17:59:50 +0530

    Create Charachters_in_C_1.c

---
+++ Charachters_in_C_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    char new_char;
+    new_char = 'T';
+    printf("%c\n", new_char);
+    
+    char next_char[3] = "GTX";
+    printf("%s\n", next_char);
+}
