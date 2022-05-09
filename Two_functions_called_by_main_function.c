386a71bc35a76ec3f9bbd776a12683a523871ced
GTHAP <40315835+GTHAP@users.noreply.github.com>
2022-05-09 18:37:15 +0530

    Create Two_functions_called_by_main_function.c

---
+++ Two_functions_called_by_main_function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void print()
+{
+    printf("PRINT TEXT HERE");
+}
+
+int add(int *a, int *b)
+{
+    int c = (*a) + (*b);
+    return c;
+}
+
+void main()
+{
+    int a = 1000, b = 999;
+    int c = add(&a, &b);
+    print(); // This may cause issues in some compilers. As the memory allocated for the add function is cleared and allocated for the print function.
+             // Use dynamic/heap memory allocation in such a scenario.
+             // This worked fine in onlinegdb. 
+    printf(" %d", c);
+}
